5c3b710f89fda25386de60e2390d7b1612cec98f
prada <epradavazquez@gmail.com>
2022-08-22 10:33:42 +0200

    Ultima ejecucion con nombres propios de deportistas

--- src/script.py
+++ src/script.py
@@ -1,4 +1,7 @@
+import time
+
 import requests
+import yaml
 from selenium.webdriver.support.wait import WebDriverWait
 from seleniumwire import webdriver
 from selenium.webdriver import Keys
@@ -8,7 +11,6 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions
 import pandas
 import os
-import time
 
 if not os.path.exists('noticias'):
     os.mkdir('noticias')
@@ -16,18 +18,27 @@ if not os.path.exists('noticias'):
 csv = pandas.read_csv('busquedas.csv')
 
 dictionary = {'ID': [], 'Termino': [], 'Fecha': [], 'Medio': [], 'Titular': [], 'Enlace local': [],
-              'Enlace intenet': []}
+              'Enlace intenet': [], 'Tam': []}
+config = yaml.safe_load(open('config/config.yaml', 'r', encoding='utf-8'))
+medios_mapping = yaml.safe_load(open('config/medios_mapping.yaml', 'r', encoding='utf-8'))
+terminos_mapping = yaml.safe_load(open('config/terminos_mapping.yaml', 'r', encoding='utf-8'))
+termino_anterior = None
+driver = None
+
 for row in csv.iterrows():
-    driver = webdriver.Firefox()
-    driver.get('https://us--mynews--es.us.debiblio.com/hu/')
-    driver.find_element(By.ID, 'edit-name').send_keys('')
-    driver.find_element(By.ID, 'edit-pass').send_keys('' + Keys.ENTER)
-    WebDriverWait(driver, 10).until(
-        expected_conditions.url_to_be('https://us--mynews--es.us.debiblio.com/hu/'))
+    termino_actual = row[1]["search_term"]
+    if not termino_actual == termino_anterior:
+        if driver:
+            driver.quit()
+        driver = webdriver.Firefox()
+        driver.get('https://us--mynews--es.us.debiblio.com/hu/')
+        driver.find_element(By.ID, 'edit-name').send_keys('')
+        driver.find_element(By.ID, 'edit-pass').send_keys('' + Keys.ENTER)
+        WebDriverWait(driver, 10).until(
+            expected_conditions.url_to_be('https://us--mynews--es.us.debiblio.com/hu/'))
 
     driver.get('https://us--mynews--es.us.debiblio.com/hu/busqueda/profesional/')
-    # driver.find_element(By.ID, 'busqueda_booleana').send_keys(row[1]['search_term'].replace(' ', ' AND '))
-    driver.find_element(By.ID, 'busqueda_booleana').send_keys(f'\"{row[1]["search_term"]}\"')
+    driver.find_element(By.ID, 'busqueda_booleana').send_keys(terminos_mapping[row[1]["search_term"]])
     driver.find_element(By.ID, 'selectorDatesButton').click()
 
     driver.find_element(By.CLASS_NAME, 'sel_date_2.cell.small-10.no-padding-left.pointer').click()
@@ -49,18 +60,8 @@ for row in csv.iterrows():
 
     driver.find_element(By.ID, 'selectorMitjans').click()
     driver.find_element(By.ID, 'selector_check_all').click()  # Deseleccionamos todos los medios
-    driver.find_element(By.ID, 'selector_check_379').click()  # Marca
-    driver.find_element(By.ID, 'selector_check_536').click()  # Marca.com
-    driver.find_element(By.ID, 'selector_check_170').click()  # El pais
-    driver.find_element(By.ID, 'selector_check_163').click()  # El mundo
-    driver.find_element(By.ID, 'selector_check_964').click()  # Sport
-    driver.find_element(By.ID, 'selector_check_914').click()  # Sport.es
-    driver.find_element(By.ID, 'selector_check_22').click()  # As
-    driver.find_element(By.ID, 'selector_check_915').click()  # As.com
-    driver.find_element(By.ID, 'selector_check_371').click()  # La vanguardia
-    driver.find_element(By.ID, 'selector_check_4').click()  # ABC
-    driver.find_element(By.ID, 'selector_check_436').click()  # abc.es
-    driver.find_element(By.ID, 'selector_check_876').click()  # eldiario.es
+    for medio in config['medios']:
+        driver.find_element(By.ID, medios_mapping[medio]).click()
 
     driver.find_element(By.ID, 'enviarPublicacions').click()
 
@@ -72,10 +73,10 @@ for row in csv.iterrows():
 
     driver.find_element(By.ID, 'search_button').click()
     try:
-        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, 'llistaNoticies1')))
+        WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.ID, 'llistaNoticies')))
     except Exception as e:
         pass
-    url, headers = None, None
+    time.sleep(1)
     for i in reversed(range(len(driver.requests))):
         if 'noticies' in driver.requests[i].url:
             url = driver.requests[i].url
@@ -89,10 +90,20 @@ for row in csv.iterrows():
     if not os.path.exists(path):
         os.mkdir(path)
     for noticie in response['noticies']:
+        titular = noticie['Title']
+        date = noticie['date']
+        medio = noticie['Newspaper']
+        if titular in dictionary['Titular']:
+            indexes = [index for index, v in enumerate(dictionary['Titular']) if v == titular]
+            boolean = False
+            for index in indexes:
+                if date == dictionary['Fecha'][index] and medio == dictionary['Medio'][index]:
+                    boolean = True
+            if boolean:
+                continue
         year = noticie['date'][-4:]
         if not os.path.exists(f'{path}/{year}'):
             os.mkdir(f'{path}/{year}')
-        medio = noticie['Newspaper']
         if not os.path.exists(f'{path}/{year}/{medio}'):
             os.mkdir(f'{path}/{year}/{medio}')
         id_document = noticie['IdDocument']
@@ -100,26 +111,37 @@ for row in csv.iterrows():
             response_pdf = None
             i = 1
             while not response_pdf:
-                print(f'{i}: {id_document}')
+                print(f'{i}: {id_document}\n')
                 response_pdf = requests.get(url=url_format.format(id_document), headers=headers)
                 i += 1
+                response_pdf.raise_for_status()
         except Exception as e:
             print('\n', e, '\n', id_document, '\n')
             continue
+        tam = len(response_pdf.content)
+        if tam in dictionary['Tam']:
+            indexes = [index for index, v in enumerate(dictionary['Tam']) if v == tam]
+            for index in indexes:
+                boolean = False
+                if date == dictionary['Fecha'][index] and medio == dictionary['Medio'][index]:
+                    boolean = True
+            if boolean:
+                continue
         pdf = open(f'{path}/{year}/{medio}/{id_document}.pdf', 'wb')
         pdf.write(response_pdf.content)
         pdf.close()
         dictionary['ID'].append(id_document)
-        dictionary['Titular'].append(noticie['Title'])
-        dictionary['Fecha'].append(noticie['date'])
+        dictionary['Titular'].append(titular)
+        dictionary['Fecha'].append(date)
         dictionary['Termino'].append(row[1]['search_term'])
         dictionary['Enlace local'].append(f'{path}/{year}/{medio}/{id_document}.pdf')
         dictionary['Medio'].append(medio)
         url_noticie = noticie['Page'] if 'http' in noticie['Page'] else None
         dictionary['Enlace intenet'].append(url_noticie)
+        dictionary['Tam'].append(tam)
     noticias = pandas.DataFrame(data=dictionary)
     noticias.to_csv('tabla_access.csv', index=False, sep=';')
-    driver.quit()
+    termino_anterior = termino_actual
 
 noticias = pandas.DataFrame(data=dictionary)
 noticias.to_csv('tabla_access.csv', index=False, sep=';')
